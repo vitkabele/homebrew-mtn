6ac912a7173816f6fc5d0c74d4005715173436a9
Raoul Schaffner <raoul.schaffner@eawag.ch>
2024-04-21 14:52:56 +0200

    Update mtn.rb
    version 3.5.0 and two-step make

--- Formula/mtn.rb
+++ Formula/mtn.rb
@@ -1,14 +1,17 @@
 class Mtn < Formula
   desc "Saves thumbnails (screenshots) of movie or video files to jpeg files"
   homepage "https://gitlab.com/movie_thumbnailer/mtn/wikis/home"
-  url "https://gitlab.com/movie_thumbnailer/mtn/-/archive/v3.4.2/mtn-v3.4.2.tar.gz"
-  sha256 "19b2076c00f5b0ad70c2467189b17f335c6e7ece5d1a01ed8910779f6a5ca52a"
+  url "https://gitlab.com/movie_thumbnailer/mtn/-/archive/v3.5.0/mtn-v3.5.0.tar.gz"
+  sha256 "0354e598dcbd3b7ac7f44e6c8b77699c5c7b51691b23e711513d785c3af06933"
 
   depends_on "ffmpeg"
   depends_on "gd"
 
   def install
-    system "make", "GB_F_FONTNAME=Supplemental/Tahoma Bold.ttf", "-Csrc", "install", "PREFIX=#{prefix}"
+    cd "src" do
+      system "make", "GB_F_FONTNAME=Supplemental/Tahoma Bold.ttf"
+      system "make", "install", "PREFIX=#{prefix}"
+    end
   end
 
   test do
